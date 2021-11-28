1dc461a8d2794e765fc2574a4f8b79bd10a519c6
Viet Anh Le <mc.vietanh@gmail.com>
2021-11-28 17:40:38 -0500

    done 11.5.3

---
+++ src/components/AddHeroBtn.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name: 'add-hero-btn',
+  props: {
+    text: {
+      type: String,
+      default: 'Add Hero',
+    },
+  },
+};
+</script>
+
+<template>
+  <a
+    class="f6 link dim ph3 pv2 mb2 dib white bg-light-purple"
+    href="#0"
+    @click="$emit('add-hero', true)"
+    >{{ text }}</a
+  >
+</template>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import tachyons from 'tachyons';
 
-Vue.config.productionTip = false
-
+Vue.use(tachyons);
+Vue.config.productionTip = false;
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
